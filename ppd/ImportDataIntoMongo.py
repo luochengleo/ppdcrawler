b4903a11d3b11ef1670a78196fc1e3de89c3169e
luocheng <luochengleo@gmail.com>
2015-06-30 14:03:24 +0800

    add

--- ppd/ImportDataIntoMongo.py
+++ ppd/ImportDataIntoMongo.py
@@ -11,7 +11,7 @@ class WebPage(Document):
     time  = StringField()
     content = StringField()
 
-connect('ppdcrawler', host='127.0.0.1', port=27017)
+connect('ppdcrawler', host='172.29.33.103', port=27017)
 
 import os
 import sys
